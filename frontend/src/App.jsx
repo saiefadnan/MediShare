1c9a211811abce38d4cf4b4d94f41409a565390c
sayef312469 <202214069@student.mist.ac.bd>
2024-12-25 23:44:54 +0600

    Login page v1.0.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 import Donation from './pages/Donation';
+import LoginPage from './pages/Login';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './styles/App.css';
 import { Suspense } from 'react';
@@ -14,6 +15,7 @@ function App() {
             <Routes>
               <Route path='/home' element={<Home/>}/>
               <Route path='/donation' element={<Donation/>}/>
+              <Route path='/login' element={<LoginPage/>}/>
             </Routes>
           </Suspense>
         </div>
