d5d6ce477709a028acc68875a483d036382c240d
sayef312469 <202214069@student.mist.ac.bd>
2024-12-28 03:18:12 +0600

    Login page+Sign up v2.0.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@ import Navbar from './components/navbar';
 import Donation from './pages/Donation';
 import Ai from './pages/Ai';
 import LoginPage from './pages/Login';
-import SignupPage from './pages/Signup';
 import FindMedPage from './pages/findMedPage/FindMedPage.jsx';
-import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
 import { Suspense } from 'react';
 import Admin from './pages/Admin.jsx';
 
 function App() {
   const location = useLocation();
-  const hideNavbarRoutes = ['/login','/signup', '/admin'];
+  const hideNavbarRoutes = ['/login', '/admin'];
   // const shouldHideNavbar = hideNavbarRoutes.some(route=>{
   //   return (route === location.pathname || location.pathname.startsWith(`${route}/`))
   // })
@@ -20,13 +20,11 @@ function App() {
           {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
           <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-              <Route path='/' element={<Navigate to='/login'/>}/>
+              <Route path='/' element={<Home/>}/>
               <Route path='/donation' element={<Donation/>}/>
               <Route path='/login' element={<LoginPage/>}/>
               <Route path='/findMed' element={<FindMedPage/>}/>
-              <Route path='/signup' element={<SignupPage/>}/>
               <Route path='/ai' element={<Ai/>}/>
-
               {/* Admin DashBoard pages */}
               <Route path='/admin/*'  element={<Admin/>}/>
               {/* Admin DashBoard pages */}
