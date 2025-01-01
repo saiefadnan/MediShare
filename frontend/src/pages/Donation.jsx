a87f9938c70cd3d6e4ee97c03c876f0c73fcbb06
sohan1878 <rayspectrum37@gmail.com>
2025-01-01 12:45:28 +0600

    v-101

--- frontend/src/pages/Donation.jsx
+++ frontend/src/pages/Donation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/style.css';
+import '../styles/donation.css';
 import Footer from '../components/Footer.jsx'
 import '../styles/footer.css'
 
