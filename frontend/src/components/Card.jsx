60c4a8beb59911244e9aafe93352136d4f24debc
sohan1878 <rayspectrum37@gmail.com>
2025-02-02 11:59:09 +0600

    update v.05

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,76 @@
 import '../styles/card.css';
 import React,{useState,useEffect} from 'react';
 
+    
+
+import axios from 'axios';
 export default function card(props){
+
 
 const [requestPopup,setRequestPopup]=useState(false);
 
 const [detailsPopup,setDetailsPopup]=useState(false);
 const [background,setBackground]=useState(true);
+
+//request form states
+
+const [requesterId,setRequesterId]=useState(props.userId);
+const [medId,setMedId]=useState(props.medId);
+const [donorId,setDonorId]=useState(props.donorId);
+const [prescriptionImg,setPrescriptionImg]=useState('');
+const [reason,setReason]=useState('');
+const [reqrQty,setReqrQty]=useState(0);
+const [file,setFile]=useState(null);
+
+const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    
+  };
+  
+const handleRequestSubmission=async (e)=>{
+    e.preventDefault();
+    if(!requesterId){
+        window.alert("Please Log In First!");
+        
+        return;
+    }
+    if(!reason||!file||!reqrQty){
+        console.log(reason);
+
+        window.alert("Fill all the fields!");
+        return;
+    }
+    const formData=new FormData();
+    formData.append('requester_id',requesterId);
+    formData.append('med_id',medId);
+    formData.append('donor_id',donorId);
+    formData.append('reason',reason);
+    formData.append('file',file);
+    formData.append('quantity',reqrQty);
+
+    
+    try {
+        
+        const  response=await axios.post('http://localhost:5000/api/requestSubmission',formData,{
+            headers:{
+                "Content-Type":'multipart/form-data'
+            },
+        });
+        if(response.status===200){
+            window.alert("Request Submitted Successfully!");
+        }else{
+            window.alert("something went wrong!");
+            console.log(response.status);
+        }
+        
+    } catch (error) {
+        window.alert("something went wrong!");
+        console.error(error);
+    }
+    setRequestPopup(false);
+    setBackground(true);
+
+}
 
 function handleRequest(){
     setRequestPopup(true);
@@ -44,16 +108,16 @@ function handleClose() {
                 requestPopup&&(
 
                     <div className='request-popup'>
-                         <form className='request-form ' action="">
-                         <input type="text"  placeholder='Reason for procuring' />
-                         <input type="text" placeholder='Your Current Location' />
+                         <form onSubmit={handleRequestSubmission} className='request-form ' action="">
+                         <input type="text"  placeholder='Reason for procuring' onChange={(e)=>setReason(e.target.value)} />
+                         
                          <span>Upload Prescription</span>
-                         <input type="file"  placeholder='Prescription' />
-                         <input type="number" placeholder='Quantity' />
+                         <input type="file" onChange={handleFileChange}  placeholder='Prescription'  />
+                         <input type="number" placeholder='Quantity' onChange={(e)=>setReqrQty(e.target.value)} />
                 
 
                              <div className='form-btns'>
-                         <button onClick={handleRequest}>Request</button>
+                         <button type='submit'>Request</button>
                         <button onClick={handleClose}>Close</button>
                         </div>
 
--- frontend/src/pages/findMedPage/FindMedPage.jsx
+++ frontend/src/pages/findMedPage/FindMedPage.jsx
@@ -4,103 +4,108 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Card from '../../components/Card.jsx';
 import { useState, useEffect } from 'react';
+import { useAuth } from '../../Contexts/AuthContext.jsx';
 
 
 
-
-// Dynamically import all images from the folder
 const images = Object.values(import.meta.glob('./assets/img/*.{png,jpg,jpeg,svg}', { eager: true })).map((mod) => mod.default);
 
 export default function FindMedPage() {
+
+    const {user}=useAuth();
+    const userId=user?.id;
+    
     const rng = 15;
 
     const [filterButton, setFilterButton] = useState(false);
-   
+
     const [count, setCount] = useState(1);
-   
-    const [data,setData]=useState([]);
+
+    const [data, setData] = useState([]);
 
 
-    const [searchKey,setSearchKey]=useState('');
-    const [medicines,setMedicines]=useState([]);
-   const [suggesion,setSuggesion]=useState([]);
-   const [suggesionOn,setSuggesionOn]=useState(false);
-   
+    const [searchKey, setSearchKey] = useState('');
+    const [medicines, setMedicines] = useState([]);
+    const [suggesion, setSuggesion] = useState([]);
+    const [suggesionOn, setSuggesionOn] = useState(false);
 
-   //filter states
 
-   const [location,setLocation]=useState('');
-   const [disease,setDisease]=useState('');
-   const [company,setCompany]=useState('');
-   const [expiryDate,setExpiryDate]=useState('');
+    //filter states
 
- 
-    
+    const [location, setLocation] = useState('');
+    const [disease, setDisease] = useState('');
+    const [company, setCompany] = useState('');
+    const [expiryDate, setExpiryDate] = useState('');
 
-    useEffect(()=>{
+
+
+
+    useEffect(() => {
 
         fetchData(searchKey);
-        
 
-    },[searchKey]);
-   
 
-  useEffect(()=>{
-    if(!searchKey) setMedicines(data.slice(0,rng));
+    }, [searchKey]);
 
-  },[data]);
 
-  useEffect(()=>{
-    if(!searchKey){setSuggesionOn([]);
-        setSuggesionOn(false);
-    } 
-    else {setSuggesion(data.slice(0,6));
-        
-    }
-    
-  },[data]);
+    useEffect(() => {
+        if (!searchKey) setMedicines(data.slice(0, rng));
+
+    }, [data]);
+
+    useEffect(() => {
+        if (!searchKey) {
+            setSuggesionOn([]);
+            setSuggesionOn(false);
+        }
+        else {
+            setSuggesion(data.slice(0, 6));
+
+        }
+
+    }, [data]);
 
 
     const fetchData = async (key = '') => {
         try {
-            
+
             const response = await axios.get(`http://localhost:5000/api/searchMedicine`, { params: { searchKey: key } });
-             setData(response.data);
-             
-             
-            
-           
-            
+            setData(response.data);
+
+
+
+
+
 
         } catch (error) {
             console.error('Error fetching medicines:', error);
         }
     };
 
-    
 
-    function handleSearch(){
-        setMedicines(data.slice(0,rng));
-        
-        
+
+    function handleSearch() {
+        setMedicines(data.slice(0, rng));
+
+
     };
 
-    function handleSearchChange(e){
-        
+    function handleSearchChange(e) {
+
         setSearchKey(e.target.value);
         setSuggesionOn(true);
- 
-        
+
+
     };
 
-    
+
 
     function loadMore() {
-        
-        
-            setCount(count + 1);
-           
-         
+
+
+        setCount(count + 1);
+
+
     }
 
     function handleFilterButton() {
@@ -108,90 +113,90 @@ export default function FindMedPage() {
     }
 
     useEffect(() => {
-        
+
         let nxtRng = count * rng;
         if (nxtRng > data.length) {
             nxtRng = data.length;
         }
-        
+
         setMedicines(data.slice(0, nxtRng));
     }, [count]);
 
-function disableSuggesion(){
-    setTimeout(() => {
-        setSuggesionOn(false);
-    }, 100);
-    
-}
+    function disableSuggesion() {
+        setTimeout(() => {
+            setSuggesionOn(false);
+        }, 100);
 
-function handleSuggestionClick(itm){
-    
-    setSearchKey(itm.common_name);
-
-    setCount(1);
-    
-    
-    
-    setMedicines(data.slice(0,rng));
-    setSuggesion([]);
-    setSuggesionOn(false);
-    
-    
-}
-
-
-async function handleFilter(e){
-    e.preventDefault();
-    setFilterButton(false);
-    let filters={};
-    if(location)    filters.location=location;
-    if(disease)     filters.disease=disease;
-    if(company)     filters.company=company;
-    if(expiryDate)   filters.expiry_date=expiryDate;
-    
-    try {
-        const response=await axios.get('http://localhost:5000/api/searchFilteredMedicine',{params:filters});
-
-        console.log(response.data);
-        setMedicines(response.data);
-
-        
-    } catch (error) {
-        console.error(error);
     }
-    setLocation(''); 
-    setDisease('');
-    setCompany('');
-    setExpiryDate('');
+
+    function handleSuggestionClick(itm) {
+
+        setSearchKey(itm.common_name);
+
+        setCount(1);
 
 
-}
+
+        setMedicines(data.slice(0, rng));
+        setSuggesion([]);
+        setSuggesionOn(false);
+
+
+    }
+
+
+    async function handleFilter(e) {
+        e.preventDefault();
+        setFilterButton(false);
+        let filters = {};
+        if (location) filters.location = location;
+        if (disease) filters.disease = disease;
+        if (company) filters.company = company;
+        if (expiryDate) filters.expiry_date = expiryDate;
+
+        try {
+            const response = await axios.get('http://localhost:5000/api/searchFilteredMedicine', { params: filters });
+
+            console.log(response.data);
+            setMedicines(response.data);
+
+
+        } catch (error) {
+            console.error(error);
+        }
+        setLocation('');
+        setDisease('');
+        setCompany('');
+        setExpiryDate('');
+
+
+    }
 
 
     return (
         <div id="full-page">
             <div className="search-section">
                 <h1>Find Your Med!</h1>
-                <div  className="search-container">
-                    <input onBlur={disableSuggesion} type="text" value={searchKey}  onChange={handleSearchChange}   placeholder="Enter Medicine Name" />
-                    <button onClick={handleSearch}  className="search-icon">
+                <div className="search-container">
+                    <input onBlur={disableSuggesion} type="text" value={searchKey} onChange={handleSearchChange} placeholder="Enter Medicine Name" />
+                    <button onClick={handleSearch} className="search-icon">
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
-                    <div  className={suggesionOn?"suggesion active":"suggesion inactive"}>
-                     
+                    <div className={suggesionOn ? "suggesion active" : "suggesion inactive"}>
+
                         {
-                            suggesion.map((itm)=>
-                                
-                               (<h4  onClick={()=>handleSuggestionClick(itm)}  key={itm.med_id} ><span >Generic Name:{itm.generic_name}</span> <span>Common Name:{itm.common_name}</span></h4>)
+                            suggesion.map((itm) =>
+
+                                (<h4 onClick={() => handleSuggestionClick(itm)} key={itm.med_id} ><span >Generic Name:{itm.generic_name}</span> <span>Common Name:{itm.common_name}</span></h4>)
 
                             )
                         }
 
 
+                    </div>
+
                 </div>
-                    
-                </div>
-               
+
             </div>
             <div className="filter-container">
                 <button onClick={handleFilterButton} className="dropdown-button">
@@ -199,40 +204,44 @@ async function handleFilter(e){
                 </button>
                 <div className={filterButton ? 'popup dropdown-active' : 'popup dropdown-inactive'}>
                     <form className="filter-form" onSubmit={handleFilter}>
-                        <input id='location' type="text" placeholder="Location" value={location} onChange={(e)=>setLocation(e.target.value)} />
-                       
-                        <input id='disease' type="text" placeholder="Disease" value={disease} onChange={(e)=>setDisease(e.target.value)} />
-                        <input id='company' type="text" placeholder="Company" value={company} onChange={(e)=>setDisease(e.target.value)} />
+                        <input id='location' type="text" placeholder="Location" value={location} onChange={(e) => setLocation(e.target.value)} />
+
+                        <input id='disease' type="text" placeholder="Disease" value={disease} onChange={(e) => setDisease(e.target.value)} />
+                        <input id='company' type="text" placeholder="Company" value={company} onChange={(e) => setDisease(e.target.value)} />
                         <label htmlFor="earliest-expiry-date">Earliest Expiry Date</label>
-                        <input type="date" id='earliest-expiry-date' value={disease} onChange={(e)=>setExpiryDate(e.target.value)} placeholder="Earliest Expiry Date" />
+                        <input type="date" id='earliest-expiry-date' value={disease} onChange={(e) => setExpiryDate(e.target.value)} placeholder="Earliest Expiry Date" />
                         <button type='submit'>Filter</button>
                     </form>
                 </div>
             </div>
             <div className="card-section">
-            <div className="cards-container">
-    {medicines.length > 0 ? (
-        medicines.map((med, index) => (
-            <Card
-                key={med.med_id}
-                imgSrc={images[index % images.length]} // Use modulo for cycling through images
-                title={med.common_name}
-                qty={med.quantity}
-                expiryDate={med.expiry_date}
-                company={med.company}
-                disease={med.disease}
-                location={med.location}
-            />
-        ))
-    ) : (
-        <h1 className="no-data" style={{marginBottom:'50px'}}>No Medicine Found!</h1>
-    )}
-</div>
-               
-                    <button className="loadmore-btn" onClick={loadMore}>
-                        Load More
-                    </button>
-                
+                <div className="cards-container">
+                    {medicines.length > 0 ? (
+                        medicines.map((med, index) => (
+                            <Card
+                                key={med.med_id}
+                                userId={userId}
+                                medId={med.med_id}
+                                donorId={med.donor_id}
+                                imgSrc={images[index % images.length]} // Use modulo for cycling through images
+                                title={med.common_name}
+                                qty={med.quantity}
+                                expiryDate={med.expiry_date}
+                                company={med.company}
+                                disease={med.disease}
+                                location={med.location}
+
+                            />
+                        ))
+                    ) : (
+                        <h1 className="no-data" style={{ marginBottom: '50px' }}>No Medicine Found!</h1>
+                    )}
+                </div>
+
+                <button className="loadmore-btn" onClick={loadMore}>
+                    Load More
+                </button>
+
             </div>
         </div>
     );
